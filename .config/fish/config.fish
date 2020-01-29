d40c0acb9a220a310de638a40a53f1f44f0697a3
enokawa <enokawa00@gmail.com>
2020-01-29 22:04:18 +0900

    aws2_completer

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,4 @@
 # aliases
-alias l        "ls"
-alias g        "git"
 alias gd       "git diff"
 alias gb       "git branch"
 alias gl       "git log"
@@ -11,3 +9,8 @@ alias identity "aws sts get-caller-identity"
 alias be       "bundle exec"
 alias dc       "docker-compose"
 alias k        "kubectl"
+
+
+# Complete aws-cli
+
+complete --command aws --no-files --arguments '(begin; set --local --export COMP_SHELL fish; set --local --export COMP_LINE (commandline); aws2_completer | sed \'s/ $//\'; end)'
