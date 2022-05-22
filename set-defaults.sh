ca05742d6a2908ff112786d6fa82baeec446f5ef
enokawa <enokawa00@gmail.com>
2022-05-22 15:08:14 +0900

    Add defaults setting

---
+++ set-defaults.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Show hidden files in Finder
+defaults write com.apple.finder AppleShowAllFiles -boolean true
+killall Finder
+
+# Enable continues input when press and hold key
+defaults write -g ApplePressAndHoldEnabled -boolean true
+
+# Disable shadow of screen capture
+defaults write com.apple.screencapture disable-shadow -boolean true
